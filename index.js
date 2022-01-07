b0d3a9a765496ded90ac1ad5482629166c91649f
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-01-07 19:33:28 +0000

    Added express code to index.js - movies list, documentation function, error function and listen

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express"),
+  morgan = require("morgan");
+const app = express();
+
+let topMovies = [
+  {
+    title: "Coco",
+    studio: "Disney",
+  },
+  {
+    title: "Luca",
+    studio: "Disney",
+  },
+  {
+    title: "Soul",
+    studio: "Disney",
+  },
+  {
+    title: "Toy Story",
+    studio: "Disney",
+  },
+  {
+    title: "Monsters Inc",
+    studio: "Disney",
+  },
+  {
+    title: "The Incredibles",
+    studio: "Disney",
+  },
+  {
+    title: "WALL-E",
+    studio: "Disney",
+  },
+  {
+    title: "Cars",
+    studio: "Disney",
+  },
+  {
+    title: "Finding Nemo",
+    studio: "Disney",
+  },
+  {
+    title: "Ratatouille",
+    studio: "Disney",
+  },
+];
+
+app.use(morgan("common"));
+
+app.get("/", function (req, res) {
+  res.send("Welcome to my movies list!");
+});
+
+app.get("/movies", function (req, res) {
+  res.json(topMovies);
+});
+
+app.use("/documentation", express.static("public"));
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(8080, function () {
+  console.log("My app is running.");
+});
