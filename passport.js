e033ab0523142b51f35dffa5e4eff608b9929f63
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-01-24 19:57:10 +0000

    Installed passport and created passport.js

---
+++ passport.js
@@ -0,0 +1,51 @@
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
+
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "Username",
+      passwordField: "Password",
+    },
+    function (username, password, callback) {
+      Users.findOne({ Username: username }, function (error, user) {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+        if (!user) {
+          console.log("incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or passwort",
+          });
+        }
+        console.log("finished");
+        return callback(null, user);
+      });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "your_jwt_secret",
+    },
+    function (jwtPayload, callback) {
+      return Users.findById(jwtPayload._id)
+        .then(function (user) {
+          return callback(null, user);
+        })
+        .catch(function (error) {
+          return callback(error);
+        });
+    }
+  )
+);
